5aee3a5933427445bd49ece4028d84494c918adb
nonouille <arnaud.py@edu.devinci.fr>
2024-03-22 12:09:10 +0100

    OPRF work

--- Part2/ClientOPRF.py
+++ Part2/ClientOPRF.py
@@ -1,18 +1,15 @@
-from cryptography.hazmat.primitives.asymmetric import dh, ec
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
-from OPRF_var import q
+from OPRF_var import q,p
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
-import csv
+import csv, random, sys
+sys.set_int_max_str_digits(99999999)
 
 app = Flask(__name__)
 
 CORS(app)
-
-r = None
-z = None
 
 # Function to hash an integer to an element of the cyclic group G
 def string_to_integer(P,q):
@@ -30,37 +27,50 @@ def H(P):
     H_P = pow(s, 2, 2*q + 1)
     return H_P
 
-def generate_randome_scalar():
-    private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
-    return private_key.private_numbers().private_value
+def generate_random_scalar():
+    scalar = random.randint(2, 99999)
+    return scalar
 
 def initialize(P,q):
     H_P = H(P)
-    r = generate_randome_scalar()
-    C = pow(H_P, r)
-    return C
+    r = generate_random_scalar()
+    C = pow(H_P, r,p)
+    return C,r
 
 @app.route('/oprf', methods=['POST'])
 def clientOPRF():
     try:
         U = request.json.get('username')
         P = request.json.get('password')
-        C = initialize(P, q)
-        
+        C,r = initialize(P, q)
+        print("C : ",C)
         body = {
             'U' : U,
             'C': C
         }
         response = requests.post('http://localhost:5000/oprf', json=body)
         R = response.json().get('R')
-        z = pow(z,-1)
-        K = pow(R, z)
+        print("R : ",R)
+        print('Type of r : ',type(r))
+        print("Type of p : ",type(p))
+        try :
+            z = pow(r,-1,p)
+            print("z : ",z)
+        except Exception as e:
+            return jsonify({'Message' : 'Error in computing z', 'Error': str(e)}), 500
+        
+        try:
+            K = pow(R, z,p)
+            print("K : ",K)
+        except Exception as e:
+            return jsonify({'Message' : 'Error in computing K', 'Error': str(e)}), 500
+        
         with open('client.csv', mode='a') as file:
             writer = csv.writer(file)
             writer.writerow([U, P, K])
-        return jsonify({'Message' : 'OPRF worded'}), 200
+        return jsonify({'Message' : 'OPRF worked'}), 200
     except Exception as e:
-        return jsonify({'Message' : 'Error in OPRF', 'Error': str(e)}), 500
+        return jsonify({'Message' : 'Error in Client OPRF', 'Error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
--- Part2/ServerOPRF.py
+++ Part2/ServerOPRF.py
@@ -1,8 +1,8 @@
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
-import csv
+from OPRF_var import p
+import os, csv
 app = Flask(__name__)
 
 CORS(app)
@@ -17,16 +17,15 @@ def serverOPRF():
         s = generate_random_salt()
         U = request.json.get('U')
         C = request.json.get('C')
-        R = pow(C, s)
+        print("s : ", s)
         with open('server.csv', mode='a') as file:
                 writer = csv.writer(file)
                 writer.writerow([U, s])
+        print("C : ", C)
+        R = pow(C, s,p)
         return jsonify({'R': R}), 200
     except Exception as e:
-        return jsonify({'Message' : 'Error in OPRF', 'Error': str(e)}), 500
-
-
-
+        return jsonify({'Message' : 'Error in Server OPRF', 'Error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
